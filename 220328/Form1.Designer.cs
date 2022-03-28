70eda9389c1df34c0b3407091dae97ad6c738715
user <user@DESKTOP-6JBEF5K>
2022-03-28 10:40:23 +0800

    加入專案檔案。

---
+++ 220328/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace _220328
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_listenPort = new System.Windows.Forms.TextBox();
+            this.textBox_receiveMsg = new System.Windows.Forms.TextBox();
+            this.textBox_targetIP = new System.Windows.Forms.TextBox();
+            this.textBox_targetPort = new System.Windows.Forms.TextBox();
+            this.textBox_sendMsg = new System.Windows.Forms.TextBox();
+            this.button_startListen = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "接聽Port";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "接收文字";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "目標IP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "目標Port";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "傳送文字";
+            // 
+            // textBox_listenPort
+            // 
+            this.textBox_listenPort.Location = new System.Drawing.Point(96, 25);
+            this.textBox_listenPort.Name = "textBox_listenPort";
+            this.textBox_listenPort.Size = new System.Drawing.Size(162, 22);
+            this.textBox_listenPort.TabIndex = 5;
+            // 
+            // textBox_receiveMsg
+            // 
+            this.textBox_receiveMsg.Location = new System.Drawing.Point(96, 64);
+            this.textBox_receiveMsg.Name = "textBox_receiveMsg";
+            this.textBox_receiveMsg.Size = new System.Drawing.Size(262, 22);
+            this.textBox_receiveMsg.TabIndex = 6;
+            // 
+            // textBox_targetIP
+            // 
+            this.textBox_targetIP.Location = new System.Drawing.Point(96, 118);
+            this.textBox_targetIP.Name = "textBox_targetIP";
+            this.textBox_targetIP.Size = new System.Drawing.Size(162, 22);
+            this.textBox_targetIP.TabIndex = 7;
+            // 
+            // textBox_targetPort
+            // 
+            this.textBox_targetPort.Location = new System.Drawing.Point(96, 155);
+            this.textBox_targetPort.Name = "textBox_targetPort";
+            this.textBox_targetPort.Size = new System.Drawing.Size(162, 22);
+            this.textBox_targetPort.TabIndex = 8;
+            // 
+            // textBox_sendMsg
+            // 
+            this.textBox_sendMsg.Location = new System.Drawing.Point(96, 190);
+            this.textBox_sendMsg.Name = "textBox_sendMsg";
+            this.textBox_sendMsg.Size = new System.Drawing.Size(262, 22);
+            this.textBox_sendMsg.TabIndex = 9;
+            // 
+            // button_startListen
+            // 
+            this.button_startListen.Location = new System.Drawing.Point(387, 64);
+            this.button_startListen.Name = "button_startListen";
+            this.button_startListen.Size = new System.Drawing.Size(75, 23);
+            this.button_startListen.TabIndex = 10;
+            this.button_startListen.Text = "啟動接收";
+            this.button_startListen.UseVisualStyleBackColor = true;
+            this.button_startListen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "傳送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 244);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_startListen);
+            this.Controls.Add(this.textBox_sendMsg);
+            this.Controls.Add(this.textBox_targetPort);
+            this.Controls.Add(this.textBox_targetIP);
+            this.Controls.Add(this.textBox_receiveMsg);
+            this.Controls.Add(this.textBox_listenPort);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_listenPort;
+        private System.Windows.Forms.TextBox textBox_receiveMsg;
+        private System.Windows.Forms.TextBox textBox_targetIP;
+        private System.Windows.Forms.TextBox textBox_targetPort;
+        private System.Windows.Forms.TextBox textBox_sendMsg;
+        private System.Windows.Forms.Button button_startListen;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 220328/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _220328
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        UdpClient U;
+        Thread Th;
+
+        //監聽副程式
+        private void Listen()
+        {
+            //設定監聽用的通訊埠
+            int Port = int.Parse(textBox_listenPort.Text);
+
+            //監聽UDP監聽器實體
+            U = new UdpClient(Port);
+
+            //建立本機端點資訊
+            IPEndPoint EP =  new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+
+            while (true)
+            {
+                byte[] B = U.Receive(ref EP);//接收到的訊息放到B陣列
+                textBox_receiveMsg.Text = Encoding.Default.GetString(B);//翻譯B陣列為字串
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;//忽略跨執行續錯誤
+            Th = new Thread(Listen);//建立監聽執行續，目標副程式 -> Listen
+            Th.Start();//啟動監聽執行續
+            button_startListen.Enabled = false;//使button失效(不能重複開啟監聽)
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Th.Abort();//關閉監聽執行續
+                U.Close();//關閉監聽器
+            }
+            catch
+            {
+                //忽略錯誤
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string IP = textBox_targetIP.Text;//設定發送目標IP
+            int Port = int.Parse(textBox_listenPort.Text);//設定發送目標Port
+            byte[] B = Encoding.Default.GetBytes(textBox_sendMsg.Text);//字串翻譯成位元
+            UdpClient S = new UdpClient();//建立UDP Client
+            S.Send(B, B.Length, IP, Port);//發送資料到指定位置
+            S.Close();///關閉UDP Client
+        }
+    }
+}
